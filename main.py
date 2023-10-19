ba5bd03f0374b2c51fc423e3ab3904bcd7c42538
logank <logan.aker1989@gmail.com>
2023-10-19 16:58:40 -0400

    Showing you some stuff.

--- main.py
+++ main.py
@@ -5,7 +5,12 @@ import os
 adjective = input('Adjective: ')
 noun = input('Noun: ')
 
+# This is a function call.
 print(f'That is one {adjective} {noun}!')
 
+# Another variable.  Notice that a variable can be a value, like 'noun', or even a function itself.
 current_directory = os.getcwd()
+
+# These functions are equivalent:
 print(current_directory)
+print(os.getcwd())
