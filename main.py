aa77c89aa60f943ababbc861c437fd375f658f3a
logank <logan.aker1989@gmail.com>
2023-10-19 17:01:10 -0400

    dice

--- main.py
+++ main.py
@@ -1,16 +1,4 @@
-# This is an import statement
-import os
+# Let's make a dice roller
 
-# This is a variable declaration
-adjective = input('Adjective: ')
-noun = input('Noun: ')
-
-# This is a function call.
-print(f'That is one {adjective} {noun}!')
-
-# Another variable.  Notice that a variable can be a value, like 'noun', or even a function itself.
-current_directory = os.getcwd()
-
-# These functions are equivalent:
-print(current_directory)
-print(os.getcwd())
+# We'll start with the following parameters:
+# Roll One Six-Sided Die and output the return
