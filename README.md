dc5929b2228c3739d0233efa78e5172a8a120281
logank <logan.aker1989@gmail.com>
2023-10-19 17:30:11 -0400

    questions

--- README.md
+++
@@ -1,2 +0,0 @@
-# jay
-jayu
--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-# Let's make a dice roller
+# The first thing you do in any Python module is to import whatever dependancies you have
 
-# We'll start with the following parameters:
-# Roll One Six-Sided Die and output the return
-
+# Think of the import statements like this: if I created a function I really like and want to reuse in THIS program, I could 
+# 'import main' in another program and get access to all of the functions in THIS program.  Does that make sense?  It's like
+# we're pulling in other Python scripts into our program so we have access to functions.  They don't all load by default, because
+# there are just too many, it'd kill the efficiency.
 import random
 
-die = random.randint(1,20)
-print(die)
+message = 'You rolled a: ' # Do we need this message saved as a variable?  Will this message ever change?
+die = random.randint(1, 20)
+
+print(message, die)
+print('You rolled a: ', die)
+
+'''Questions:
+
+1) Add an output for a second die, six-sided this time (so one d20, one d6)
+
+2) Add an output to flip a coin.
+
+3) Can you output all of these things -- 1d20, 1d6, and one coin -- as a single message?
+'''
